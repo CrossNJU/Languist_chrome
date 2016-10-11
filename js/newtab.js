bd6f61c5e091707b76ed485e95accade9de64c2c
RayChen <raychen@Ray-Chens-MacBook-Pro.local>
2016-10-11 11:53:53 +0800

    Update style

--- js/newtab.js
+++ js/newtab.js
@@ -2,15 +2,19 @@
  * Created by raychen on 2016/10/11.
  */
 document.addEventListener('DOMContentLoaded', function () {
-  var data = chrome.extension.getBackgroundPage().ret_data;
-  var repo_data = data.repos.slice(0, 6) ;
-  if(data.error){
-
-  }else{
-    $('#repo-item-tmpl').tmpl({data: repo_data}).appendTo('#repo-item-group');
-
-    Materialize.showStaggeredList('#repo-item-group');
-  }
-
+    $.ajax({
+        url: "http://localhost:3001/api/plugin/newtab",
+        cache: false,
+        type: "GET",
+        dataType: "json"
+    }).done(function (msg) {
+        console.log(msg);
+        var repo_data = msg;
+        console.log(repo_data);
+        $('#repo-item-tmpl').tmpl({data: repo_data}).appendTo('#repo-item-group');
+        Materialize.showStaggeredList('#repo-item-group');
+    }).fail(function (jqXHR, textStatus) {
+        console.log('fail');
+    });
 
 });
