9c22b2aa59944adfb9bb93d2f5e6dd1a023b60e3
chenmuen <593827475@qq.com>
2016-10-11 14:42:03 +0800

    Fix some bugs

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   var data = chrome.extension.getBackgroundPage().ret_data;
   var repo_data = data.repos;
-  if (data.error) {
 
-  } else {
-    console.log(repo_data[0]);
-    $('#repo-item-tmpl').tmpl({
-      data: repo_data
-    }).appendTo('#repo-item-group');
-  }
-
+  var timer = setInterval(()=> {
+    console.log('wait');
+    if(repo_data) {
+      console.log(repo_data[0]);
+      $('#repo-item-tmpl').tmpl({
+        data: repo_data
+      }).appendTo('#repo-item-group');
+      clearInterval(timer);
+    }
+  }, 500);
 
 });
